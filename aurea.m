283c8bf9a99af98b4e3452a0bdf6556f33162949
juannico007 <58712333+juannico007@users.noreply.github.com>
2021-11-23 10:05:02 -0500

    Update aurea.m

--- aurea.m
+++ aurea.m
@@ -1,4 +1,9 @@
 function y = aurea(f, a, b)
+    %Wrapper para una función de sección aurea
+    %INPUT: 
+    %f: función a minimizar
+    %a: limite inferior del intervalo
+    %b: limite superior del intervalo
     p = 0.618;
     la = a + (1- p)*(b - a);
     u = a + (p)*(b-a);
