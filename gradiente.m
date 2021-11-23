1ad4d9bc0fe9d8f97108c1bc6b5991b5c0dbbded
dleyvacastro <dleyvacastro@gmail.com>
2021-11-22 20:37:33 -0500

    magia oscura

--- gradiente.m
+++ gradiente.m
@@ -1,15 +1,15 @@
 function [x, y, z] = gradiente()
     syms x1 x2 x3 la
-    ro = 1;
+    ro = 1000000000000000000;
     p = [0 0 0];
 %    f = (x1-5)^2 + (x2-4)^2 +(x3-3)^2;
-    f = ((x3^4 + x2 + x1^2) + (44*x1 + 72*x2 + 95*x3) + ro*((44*x1 + 72*x2 + 95*x3 - 7000)^2 +(-40*x1 -60*x2 -80*x3 + 5700)^2+(-x1-x2-x3+75)^2+(x1 - 50)^2+(-x1+25)^2 +(x2 - 50)^2+(-x2+20)^2 +(x3 - 50)^2+(-x3+10)^2));
+    f = ((x3 + x2^2 + x1^2) + (44*x1 + 72*x2 + 95*x3) + ro*((44*x1 + 72*x2 + 95*x3 - 7000)^2 +(-40*x1 -60*x2 -80*x3 + 5700)^2+(-x1-x2-x3+75)^2+(x1 - 50)^2+(-x1+25)^2 +(x2 - 50)^2+(-x2+20)^2 +(x3 - 50)^2+(-x3+10)^2));
     e = 1;
     g(x1, x2, x3) = gradient(f);
     grad = g(p(1), p(2), p(3));
     k = 1;
     
-    while k < 10
+    while k < 9
         norm(grad);
         d = -grad;
         n = p' + la*d;
@@ -22,15 +22,15 @@ function [x, y, z] = gradiente()
         la2 = newton(h, 0);
         p = p' + la2*d;
         p = p';
-        p1 = p(1);
-        p2 = p(2);
-        p3 = p(3);
+        p1 = round(p(1));
+        p2 = round(p(2));
+        p3 = round(p(3));
         grad = g(p1, p2, p3);
         k = k+1
         
         
     end
-    x = ceil(p1);
-    y = ceil(p2);
-    z = ceil(p3);
+    x = ceil(p1)
+    y = ceil(p2)
+    z = ceil(p3)
 end
